c0b6e620ed6f1786bb5303f045e5f4ba5d3ffb12
kjmatt91 <kjmatt91@gmail.com>
2021-10-05 22:52:15 -0400

    setup a search box component file and cleaned up some of the unnecessary files

---
+++ client/src/components/Search.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const Search = () => {
+    const [searchTerm, setSearchTerm] = useState('')
+    const [novels, setNovels] = useState([])
+    useEffect(() => {
+        fetchNovels()
+    }, [])
+
+    const fetchNovels = async () => {
+        const { data } = await axios.get('http://localhost:1337/api/novels')
+        setNovels(data)
+    }
+
+    return (
+        <div className="novels">
+            <div className="name">Great Novels</div>
+            <input
+                className="searchBox"
+                type="text"
+                name="search"
+                value={ searchTerm }
+                onChange={event => setSearchTerm(event.target.value)}
+            />
+            {
+                novels.filter(novels => novels.title.toLowerCase().includes(searchTerm.toLowerCase())).map(novel => {
+                    return (<h1 key={novel.id}>{novel.title} by {novel.author.firstName} {novel.author.lastName}</h1>)
+                })
+            }
+        </div>
+    )
+}
+
+export default Search
